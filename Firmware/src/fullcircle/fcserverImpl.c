2044790eaa11622f7dfdf632d6b36318d9fc5095
0110 <github@buxs.org>
2013-10-28 19:55:15 +0100

    Initial implementation of the FCserver

--- Firmware/src/fullcircle/fcserverImpl.c
+++ Firmware/src/fullcircle/fcserverImpl.c
@@ -4,6 +4,22 @@
 #include "chprintf.h"
 #include "ch.h"
 
+#include <unistd.h>
+#include "fcserver.h"
+
+/******************************************************************************
+ * IMPLEMENTATION FOR THE NECESSARY CALLBACKS
+ ******************************************************************************/
+
+void onNewImage(uint8_t* rgb24Buffer, int width, int height)
+{
+	/* printf("%d x %d\n", width, height); */
+}
+
+/******************************************************************************
+ * GLOBAL FUNCTIONS
+ ******************************************************************************/
+
 /**
  * Stack area for the http thread.
  */
@@ -12,15 +28,30 @@ WORKING_AREA(wa_fc_server, FCSERVERIMPL_THREAD_STACK_SIZE);
 /**
  * HTTP server thread.
  */
-msg_t fc_server(void *p) {	
+msg_t fc_server(void *p)
+{		
+	fcserver_ret_t	ret;
+	fcserver_t		server;
 	chRegSetThreadName("dynfc-server");
 	(void)p;
-	
-	while(1)
+		
+	ret = fcserver_init(&server, &onNewImage, 10, 12);
+	if (ret != FCSERVER_RET_OK)
 	{
-	
-		chThdSleep(MS2ST(FCSERVER_IMPL_SLEEPTIME /* convert milliseconds to system ticks */));
+		/* printf("Server initialization failed with returncode %d\n", ret); */
+		return FR_INT_ERR;
 	}
+	
+	fcserver_setactive(&server, 1 /* TRUE */);
+	
+	do {
+		ret = fcserver_process(&server);
+		
+		chThdSleep(MS2ST(FCSERVER_IMPL_SLEEPTIME /* convert milliseconds to system ticks */));
+	} while ( ret == FCSERVER_RET_OK);
+	
+	/* clean everything */
+	fcserver_close(&server);
 		
 	return RDY_OK;
 }
