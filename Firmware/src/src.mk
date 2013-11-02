368b96fbceb51e54f62e1e9f49425a16287d84a1
tobi <mail@tobima.de>
2013-11-02 01:41:58 +0100

    update make rules

---
+++ Firmware/src/src.mk
@@ -0,0 +1,17 @@
+# List of all the board related files.
+APPSRC = src/web/web.c \
+       src/dmx/dmx.c \
+       src/dmx/dmx_cmd.c \
+       src/main.c \
+       src/fcs/fcs.c \
+       src/netstream/netstream.c \
+       src/netshell/netshell.c \
+       src/cmd/cmd_threads.c \
+       src/cmd/ifconfig.c \
+       src/cmd/cmd_mem.c \
+       src/cmd/cmd_cat.c \
+       src/ini/ini.c \
+       src/conf/conf.c
+
+# Required include directories
+APPINC = ${APP}
