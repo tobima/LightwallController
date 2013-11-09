c9427c6c6b54d7a29f64dcc2ed32cba22c1f4a58
0110 <github@buxs.org>
2013-11-09 09:50:41 +0100

    ifconfig has more detailed output; interface can be modified manually

--- Firmware/src/cmd/ifconfig.c
+++ Firmware/src/cmd/ifconfig.c
@@ -3,10 +3,13 @@
 #include "chprintf.h"
 #include "ch.h"
 
+#include <string.h>
+
 void cmd_ifconfig(BaseSequentialStream *chp, int argc, char *argv[]) {
   struct netif* netif_et0 = netif_default;
 
-  if (argc<1) { 
+  if (argc < 1)
+  { 
     chprintf(chp, 
            "%c%c%d\tHWaddr %02x:%02x:%02x:%02x:%02x:%02x\r\n",
            netif_et0->name[0],
@@ -24,18 +27,37 @@ void cmd_ifconfig(BaseSequentialStream *chp, int argc, char *argv[]) {
            ip4_addr2(&(netif_et0->ip_addr.addr)),
            ip4_addr3(&(netif_et0->ip_addr.addr)),
            ip4_addr4(&(netif_et0->ip_addr.addr)));
-  } else {
-    ip_addr_t addr;
-    addr.addr = ipaddr_addr("10.23.42.123");
-    ip_addr_t netmask;
-    netmask.addr = ipaddr_addr("255.255.255.0");
-    ip_addr_t gw;
-    gw.addr = ipaddr_addr("10.23.42.1");
-    
-    netifapi_netif_set_addr(
-      netif_et0,
-      &addr,
-      &netmask,
-      &gw);
+	  
+	chprintf(chp,
+			   "\tinet mask %d.%d.%d.%d\r\n",
+			   ip4_addr1(&(netif_et0->netmask.addr)),
+			   ip4_addr2(&(netif_et0->netmask.addr)),
+			   ip4_addr3(&(netif_et0->netmask.addr)),
+			   ip4_addr4(&(netif_et0->netmask.addr)));
+	  
+	chprintf(chp,
+			   "\tinet gw %d.%d.%d.%d\r\n",
+			   ip4_addr1(&(netif_et0->gw.addr)),
+			   ip4_addr2(&(netif_et0->gw.addr)),
+			   ip4_addr3(&(netif_et0->gw.addr)),
+			   ip4_addr4(&(netif_et0->gw.addr)));
+  }
+  else
+  {
+	  if (strcmp(argv[0], "help") == 0 || strcmp(argv[0], "--help") == 0)
+	  {
+		  chprintf(chp, "ifconfig interface [address [network mask]] [defaultgw]\r\n");
+	  }
+	  else if (strcmp(argv[0], "ms0") == 0 && argc >= 4) /* this is the only network interface */
+	  {
+		  ip_addr_t addr;
+		  addr.addr = ipaddr_addr(argv[1]);
+		  ip_addr_t netmask;
+		  netmask.addr = ipaddr_addr(argv[2]);
+		  ip_addr_t gw;
+		  gw.addr = ipaddr_addr(argv[3]);
+		  
+		  netifapi_netif_set_addr(netif_et0, &addr, &netmask, &gw);
+	  }
   }
 }; 
