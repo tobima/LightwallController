704d70c3b2e46bfc1c16ca04d2d34cf2ac81074c
c3ma <c3ma@calculator>
2014-04-25 21:10:20 +0200

    Flash can be read

--- Firmware/src/cmd/cmd_flash.c
+++ Firmware/src/cmd/cmd_flash.c
@@ -7,47 +7,9 @@
 #define PRINTABLE_CHARACTER_LAST 0x7e
 #define PRINTABLE_CHARACTER_COUNT (PRINTABLE_CHARACTER_LAST - PRINTABLE_CHARACTER_FIRST + 1)
 
-#define TEST_READWRITE_TESTCASE_COUNT 8
-
-/**
- * @brief The flash_sector_def struct describes a flash sector
- */
-struct flash_sector_def
-{
-    flashsector_t index; ///< Index of the sector in the flash memory
-    size_t size; ///< Size of the sector in bytes
-    flashaddr_t beginAddress; ///< First address of the sector in memory (inclusive)
-    flashaddr_t endAddress; ///< Last address of the sector in memory (exclusive)
-};
-
-/**
- * @brief Definition of the stm32f407 sectors.
- */
-const struct flash_sector_def f407_flash[FLASH_SECTOR_COUNT] =
-{
-    { 0, 16 * 1024, 0x08000000, 0x08004000},
-    { 1, 16 * 1024, 0x08004000, 0x08008000},
-    { 2, 16 * 1024, 0x08008000, 0x0800C000},
-    { 3, 16 * 1024, 0x0800C000, 0x08010000},
-    { 4, 64 * 1024, 0x08010000, 0x08020000},
-    { 5, 128 * 1024, 0x08020000, 0x08040000},
-    { 6, 128 * 1024, 0x08040000, 0x08060000},
-    { 7, 128 * 1024, 0x08060000, 0x08080000},
-    { 8, 128 * 1024, 0x08080000, 0x080A0000},
-    { 9, 128 * 1024, 0x080A0000, 0x080C0000},
-    { 10, 128 * 1024, 0x080C0000, 0x080E0000},
-    { 11, 128 * 1024, 0x080E0000, 0x08100000}
-};
-
-/**
- * @brief The test_flashreadwrite_data struct defines a test case data for the read/write test.
- */
-struct test_flashreadwrite_data
-{
-    char* testName; ///< Name of the test case
-    flashaddr_t from; ///< First address to be read/write (inclusive)
-    flashaddr_t to; ///< Last address to be read/write (exclusive)
-};
+#define TEST_BASE						0x08060000
+#define TEST_DATASIZE 					sizeof(flashdata_t)
+#define TEST_READWRITE_TESTCASE_COUNT	8
 
 void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
 {
@@ -61,20 +23,19 @@ void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
   (void) argc;
   (void) argv;
 
-  for (i = 0; i < FLASH_SECTOR_COUNT; ++i)
+  for (i = 0; i < TEST_READWRITE_TESTCASE_COUNT; ++i)
   {
-	  const struct flash_sector_def* sector = &f407_flash[i];
-	  chprintf(chp, "test sector %u [=? %u]\t: 0x%x -> 0x%x (%u bytes)\r\n",
-	                   sector->index,
-	                   flashSectorAt(sector->beginAddress),
-	                   sector->beginAddress,
-	                   sector->endAddress,
-	                   sector->size);
+	  int address = TEST_BASE + (i* TEST_DATASIZE);
+	  chprintf(chp, "test sector %u\t: 0x%x -> 0x%x (%u bytes)\r\n",
+	                   flashSectorAt(address),
+	                   address,
+	                   address + TEST_DATASIZE,
+	                   TEST_DATASIZE);
 
 
-	  status = flashRead(sector->beginAddress,
-	  			  readBufferBefore, sector->size);
-	  if (status == FLASH_RETURN_SUCCESS)
+	  status = flashRead(address,
+	  			  readBufferBefore, TEST_DATASIZE);
+	  if (status != FLASH_RETURN_SUCCESS)
 	  {
 		  chprintf(chp, "Reading returned %d\r\n", status);
 		  return;
