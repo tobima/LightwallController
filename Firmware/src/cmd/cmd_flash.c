c1e9dfa7267200e3381073f564e803de0e10e378
c3ma <c3ma@calculator>
2014-04-25 21:24:16 +0200

    Starting flashing

--- Firmware/src/cmd/cmd_flash.c
+++ Firmware/src/cmd/cmd_flash.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include "ch.h"
 #include "chprintf.h"
 #include "flash.h"
@@ -14,18 +16,44 @@
 void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
 {
   int j;
-  int status;
+  int status, address;
   char readBufferBefore[32];
   flashsector_t i;
   chprintf(chp, "Flash test\r\n");
 
   /* Handle warnings: */
-  (void) argc;
-  (void) argv;
+  if (argc >= 1)
+  {
+	  if (strncmp(argv[0], "write", sizeof("write")) == 0)
+	  {
+		  if (argc >= 2)
+		  {
+			  address = TEST_BASE;
+			  readBufferBefore[0] = atoi(argv[1]);
+
+			  status = flashWrite(address,
+			  	  			  readBufferBefore, TEST_DATASIZE);
+			  if (status != FLASH_RETURN_SUCCESS)
+			  {
+				  chprintf(chp, "Writing returned %d\r\n", status);
+				  return;
+			  }
+
+			  chprintf(chp, "test sector %u : stored at 0x%x %d\r\n",
+					  flashSectorAt(address),
+					  address,
+					  readBufferBefore[0]);
+		  }
+		  else
+		  {
+			  chprintf(chp, "flash needs additional parameter\r\n");
+		  }
+	  }
+  }
 
   for (i = 0; i < TEST_READWRITE_TESTCASE_COUNT; ++i)
   {
-	  int address = TEST_BASE + (i* TEST_DATASIZE);
+	  address = TEST_BASE + (i* TEST_DATASIZE);
 	  chprintf(chp, "test sector %u\t: 0x%x -> 0x%x (%u bytes)\r\n",
 	                   flashSectorAt(address),
 	                   address,
@@ -41,7 +69,7 @@ void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
 		  return;
 	  }
 
-	  for(j=0; j < 32; j++)
+	  for(j=0; j < TEST_DATASIZE; j++)
 	  {
 		  chprintf(chp, "%0.2X", readBufferBefore[j]);
 	  }
