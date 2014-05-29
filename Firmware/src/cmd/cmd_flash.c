a6d75441ff2d1d146cd851453c88c98881d4b774
Ollo <ollo@vm>
2014-05-29 23:00:04 +0200

    Formating

--- Firmware/src/cmd/cmd_flash.c
+++ Firmware/src/cmd/cmd_flash.c
@@ -13,7 +13,8 @@
 #define TEST_DATASIZE 					((int) sizeof(flashdata_t))
 #define TEST_READWRITE_TESTCASE_COUNT	8
 
-void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
+void
+cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
 {
   int j;
   int status, address;
@@ -23,58 +24,51 @@ void cmd_flash(BaseSequentialStream *chp, int argc, char *argv[])
 
   /* Handle warnings: */
   if (argc >= 1)
-  {
-	  if (strncmp(argv[0], "write", sizeof("write")) == 0)
-	  {
-		  if (argc >= 2)
-		  {
-			  address = TEST_BASE;
-			  readBufferBefore[0] = atoi(argv[1]);
+    {
+      if (strncmp(argv[0], "write", sizeof("write")) == 0)
+        {
+          if (argc >= 2)
+            {
+              address = TEST_BASE;
+              readBufferBefore[0] = atoi(argv[1]);
 
-			  status = flashWrite(address,
-			  	  			  readBufferBefore, TEST_DATASIZE);
-			  if (status != FLASH_RETURN_SUCCESS)
-			  {
-				  chprintf(chp, "Writing returned %d\r\n", status);
-				  return;
-			  }
+              status = flashWrite(address, readBufferBefore, TEST_DATASIZE);
+              if (status != FLASH_RETURN_SUCCESS)
+                {
+                  chprintf(chp, "Writing returned %d\r\n", status);
+                  return;
+                }
 
-			  chprintf(chp, "test sector %u : stored at 0x%x %d\r\n",
-					  flashSectorAt(address),
-					  address,
-					  readBufferBefore[0]);
-		  }
-		  else
-		  {
-			  chprintf(chp, "flash needs additional parameter\r\n");
-		  }
-	  }
-  }
+              chprintf(chp, "test sector %u : stored at 0x%x %d\r\n",
+                  flashSectorAt(address), address, readBufferBefore[0]);
+            }
+          else
+            {
+              chprintf(chp, "flash needs additional parameter\r\n");
+            }
+        }
+    }
 
   for (i = 0; i < TEST_READWRITE_TESTCASE_COUNT; ++i)
-  {
-	  address = TEST_BASE + (i* TEST_DATASIZE);
-	  chprintf(chp, "test sector %u\t: 0x%x -> 0x%x (%u bytes)\r\n",
-	                   flashSectorAt(address),
-	                   address,
-	                   address + TEST_DATASIZE,
-	                   TEST_DATASIZE);
+    {
+      address = TEST_BASE + (i * TEST_DATASIZE);
+      chprintf(chp, "test sector %u\t: 0x%x -> 0x%x (%u bytes)\r\n",
+          flashSectorAt(address), address, address + TEST_DATASIZE,
+          TEST_DATASIZE);
 
+      status = flashRead(address, readBufferBefore, TEST_DATASIZE);
+      if (status != FLASH_RETURN_SUCCESS)
+        {
+          chprintf(chp, "Reading returned %d\r\n", status);
+          return;
+        }
 
-	  status = flashRead(address,
-	  			  readBufferBefore, TEST_DATASIZE);
-	  if (status != FLASH_RETURN_SUCCESS)
-	  {
-		  chprintf(chp, "Reading returned %d\r\n", status);
-		  return;
-	  }
+      for (j = 0; j < TEST_DATASIZE; j++)
+        {
+          chprintf(chp, "%0.2X", readBufferBefore[j]);
+        }
+      chprintf(chp, "\r\n");
 
-	  for(j=0; j < TEST_DATASIZE; j++)
-	  {
-		  chprintf(chp, "%0.2X", readBufferBefore[j]);
-	  }
-	  chprintf(chp, "\r\n");
-
-  }
+    }
 
 }
