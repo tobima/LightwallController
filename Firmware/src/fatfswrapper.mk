1a66b9c8046cedb5cedcc116cc0ed345f5b6f852
Ollo <ollo@vm>
2014-06-11 22:09:54 +0200

    Included found wrap ping project for accessing the FAT filesystem

---
+++ Firmware/src/fatfswrapper.mk
@@ -0,0 +1,9 @@
+FATFSWRAPPER = lib/fatfswrapper
+# List of all the flash related files.
+FATFSWRAPPERSRC =  $(FATFSWRAPPER)/fatfsWrapper.c
+
+# Required include directories
+FATFSWRAPPERINC = $(FATFSWRAPPER)
+
+# List all user C define here
+FATFSWRAPPERDEFS =
